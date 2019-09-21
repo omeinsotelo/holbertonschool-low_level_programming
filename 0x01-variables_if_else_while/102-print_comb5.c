3bdf06f91588e4d7822a29d3c6b451a93d3f1e13
omeinsotelo <omeinsotelo@gmail.com>
2019-09-21 00:50:16 +0000

    Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int n;
+	int m;
+
+	for (n = 0; n <= 99; n++)
+	{
+		for (m = 0; m <= 99; m++)
+		{
+			if ((n != m) && (n < m))
+			{
+				putchar((n / 10) + '0');
+				putchar((n % 10) + '0');
+				putchar(' ');
+				putchar((m / 10) + '0');
+				putchar((m % 10) + '0');
+				if (n != 98 || m != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar ('\n');
+	return (0);
+}
