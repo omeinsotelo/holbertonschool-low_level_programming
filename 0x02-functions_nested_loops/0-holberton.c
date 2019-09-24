9711eaf5134834a826ae30378b4af04625504dc1
omeinsotelo <omeinsotelo@gmail.com>
2019-09-24 14:12:37 +0000

    _putchar

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+* main - Entry point
+* Write a program that prints Holberton, followed by a new line.
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+char H[] = "Holberton";
+int c = sizeof(H);
+int p = 0;
+
+for ( ; p < c; p++)
+{
+_putchar(H[p]);
+}
+_putchar('\n');
+return (0);
+}
