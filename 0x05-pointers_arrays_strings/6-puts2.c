ea52114e06165e3a18468943d46496feace2562c
omeinsotelo <omeinsotelo@gmail.com>
2019-10-02 00:16:47 +0000

    6. Half the lies they tell about me aren't true

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,10 +8,9 @@ void puts2(char *str)
 {
 	int a;
 
-	for (a = 0; str[a + 1] != '\0'; a++)
+	for (a = 0; str[a + 1] != '\0'; a += 2)
 	{
-		if ((str[a] - 48) % 2 == 0)
-			_putchar(str[a]);
+		_putchar(str[a]);
 	}
 	_putchar('\n');
 }
