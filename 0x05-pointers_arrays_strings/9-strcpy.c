2a28ecdb8d40ccd5735d01a5646d4160f68322f0
omeinsotelo <omeinsotelo@gmail.com>
2019-10-02 01:40:59 +0000

    to fix

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ *print_array - function that prints other characters
+ *@dest: the parameter
+ *@src: the parameter
+ *Return: none
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int a, t, b;
+
+	b = 0;
+
+	for (a = 0; s[a] != 0; a++)
+	{
+	}
+	a--;
+	while (b < a)
+	{
+		t = s[b];
+		s[b] = s[a];
+		s[a] = t;
+		b++;
+		a--;
+	}
+}
