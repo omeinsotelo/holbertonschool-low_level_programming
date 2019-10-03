250472eb595bb6dfe5dcb055e2906475546ac9f4
omeinsotelo <omeinsotelo@gmail.com>
2019-10-03 22:56:03 +0000

    3. strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ *_strcmp - compares two strings.
+ *@s1: This is the first string to be compared
+ *@s2: This is the second string to be compared
+ *Return: result
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+	int r;
+
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		if (s1[i] == s2[i])
+			r = 0;
+		else
+			r = s1[i] - s2[i];
+			break;
+	}
+
+	return (r);
+}
+
