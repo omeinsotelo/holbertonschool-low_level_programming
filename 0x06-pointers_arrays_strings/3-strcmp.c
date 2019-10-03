4c32db55873861eabd40b014906c7bcdd9f8bd6d
omeinsotelo <omeinsotelo@gmail.com>
2019-10-03 23:01:54 +0000

    3. strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,24 +1,23 @@
 #include "holberton.h"
 /**
- *_strcmp - compares two strings.
- *@s1: This is the first string to be compared
- *@s2: This is the second string to be compared
- *Return: result
+ * _strcmp - Entry point
+ * Description: function that cancatenates two strings
+ * @s1: parametre to check
+ * @s2: parametre to check
+ *
+ * Return: always r
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i;
-	int r;
+	int i, r;
 
 	for (i = 0; s1[i] != '\0'; i++)
-	{
 		if (s1[i] == s2[i])
 			r = s1[i] - s2[i];
 		else
+		{
 			r = s1[i] - s2[i];
 			break;
-	}
-
+		}
 	return (r);
 }
-
