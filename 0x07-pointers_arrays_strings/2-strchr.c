e21f86d506256d162149fa6aab4e7bb121aa96d3
omeinsotelo <omeinsotelo@gmail.com>
2019-10-07 22:17:28 +0000

    2. strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _strchr- Entry point
+ * Description: function that locates a character in a string.
+ * @s: This is the string
+ * @c: first occurrence of the character
+ * Return: a pointer of c
+ */
+char *_strchr(char *s, char c)
+{
+	int j;
+
+	for (j = 0; s[j] != '\0' ; j++)
+	{
+		if (s[j] == c)
+		{
+			return (s + j);
+		}
+	}
+	return ('\0');
+}
