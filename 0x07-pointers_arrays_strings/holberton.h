df521fdd17419d6dafd460197c423ef0c4cfe235
omeinsotelo <omeinsotelo@gmail.com>
2019-10-08 22:33:07 +0000

    5. strstr

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -42,3 +42,4 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 void set_string(char **s, char *to);
+char *_strstr(char *haystack, char *needle);
