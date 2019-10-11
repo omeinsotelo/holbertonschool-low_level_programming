994a2a065fe8d086aeb969ad86f65fcf0791718d
omeinsotelo <omeinsotelo@gmail.com>
2019-10-11 00:38:16 +0000

    Inception. Is it possible?

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * is_prime_number- Entry point
+ * Description: function that returns 1 if is a prime number
+ * @n: the number
+ * Return: 1 and 0
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+		return (rec_aux(n, 2));
+}
+
+/**
+ * rec_aux- Entrry point
+ * Description: Auxiliar Funsion
+ * @n: the number
+ * @i: the number of iterecion
+ * Return: -1, 0.
+ */
+int rec_aux(int n, int x)
+{
+	if (x == n)
+		return (1);
+	else if (n % x == 0)
+		return (0);
+	else
+		return (rec_aux(n, x + 1));
+}
--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -52,3 +52,5 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int sqrt_aux(int n, int i);
+int is_prime_number(int n);
+int rec_aux(int n, int x);
