77b9e6df3927296db16eeb5379164582a8d953b2
JHON <jhon@jhons-macbook.zte.com.cn>
2019-10-13 10:36:32 -0500

    4. To infinity and beyond

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,25 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 /**
- * main - prints the number of arguments passed into it
- * @argc: number of arguments of array
+ * main - result of add two numbers
+ * @argc: number of arguments
  * @argv: array of arguments
- * Return: 0
-*/
+ * Return: 1 and error if the numbers is not digit
+ */
 int main(int argc, char *argv[])
 {
-	int p, q;
-
-	if (argc <= 2)
+	int c;
+	int a = 0;
+	int s;
+	if (argc < 2)
 	{
-		printf("Error\n");
-		return (1);
-	}
-	else
-	{
-		p = atoi(argv[1]);
-		q = atoi(argv[2]);
-		printf("%d\n", p * q);
+		printf("0\n");
 		return (0);
 	}
+	for (c = 1; c < argc; c++)
+	{
+		for (s = 0; argv[c][s] != '\0'; s++)
+		{
+			if (!(isdigit(argv[c][s])))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		a += atoi(argv[c]);
+	}
+	printf("%d\n", a);
+	return (0);
 }
