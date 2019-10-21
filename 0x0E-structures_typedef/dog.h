4c03dc48fcde3a1fd530903b4287cf6a105c8be2
omeinsotelo <omeinsotelo@gmail.com>
2019-10-21 22:38:25 +0000

    Django

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H
+#define _DOG_H
+/**
+ * struct dog - dog
+ * @name: first member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: name, age, owner of dname, age, owner of dog
+ */
+struct dog
+{
+	char *name;
+	float *age;
+	char *owner;
+};
+#endif
