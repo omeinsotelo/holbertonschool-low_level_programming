4c620f1e858da91c4ee26f7a002476a56e5f9c7a
omeinsotelo <omeinsotelo@gmail.com>
2019-11-12 21:08:31 +0000

    Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,27 +8,27 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	char *place;
-	int valid;
+	int valido, validr, validw;
 
-	valid = open(filename, O_RDONLY);
-	if (valid == -1)
+	valido = open(filename, O_RDONLY);
+	if (valido < 0)
 	{
 		return (0);
 	}
 	place = malloc(sizeof(char) * letters);
-	if (!place)
+	if (place == NULL)
 	{
 		return (0);
 	}
-	valid = read(valid, place, letters);
-	if (valid < 0)
+	validr = read(valido, place, letters);
+	if (validr < 0)
 	{
 		return (0);
 	}
-	valid = write(STDOUT_FILENO, place, valid);
-	if (valid < 0)
+	validw = write(STDOUT_FILENO, place, validr);
+	if (validw < 0)
 	{
 		return (0);
 	}
-	return (valid);
+	return (validw);
 }
