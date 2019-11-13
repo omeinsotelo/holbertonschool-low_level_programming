f79a3947c2324192ed45f1396ca668fe70371ada
omeinsotelo <omeinsotelo@gmail.com>
2019-11-13 00:15:47 +0000

    2

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+/**
+ * append_text_to_file - function that appends text at the end of a file.
+ * @filename: the file
+ * @text_content: the content
+ * Return: 1 success, -1 on failure
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+	int counter;
+	int wr;
+
+	if (!filename)
+	{
+		return (-1);
+	}
+	if (!text_content)
+	{
+		text_content = "";
+	}
+	fd = open(filename, O_CREAT | O_WRONLY | O_APPEND, 0600);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+	for (counter = 0; text_content[counter] != '\0'; counter++)
+	{
+	}
+	wr = write(fd, text_content, counter);
+	if (wr < 0)
+	{
+		close(fd);
+		return (-1);
+	}
+	return (1);
+}
