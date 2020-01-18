d9c0e0132601c7618177d5b49589e8675257c378
omeinsotelo <omeinsotelo@gmail.com>
2020-01-18 22:12:55 +0000

    0-hash_table_create.c

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,25 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - create
+ * @size: size of the table
+ *
+ * Return: pointer to the hash table
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new_hash_table = malloc(sizeof(hash_table_t));
+
+	if (new_hash_table == NULL)
+	{
+		return (NULL);
+	}
+	new_hash_table->array = malloc(sizeof(hash_node_t *) * size);
+	if (new_hash_table->array == NULL)
+	{
+		return (NULL);
+	}
+	new_hash_table->size = size;
+	return (new_hash_table);
+}
