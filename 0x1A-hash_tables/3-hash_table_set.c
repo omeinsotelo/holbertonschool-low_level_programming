5a75d54728054602af1dcbfce0707d6dc433b848
omeinsotelo <omeinsotelo@gmail.com>
2020-01-19 00:31:11 +0000

    3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,10 +12,13 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int hash_index = 0;
 	hash_node_t *new_node = NULL, *current_node = NULL;
+	char *value_dup = NULL;
 
 	if (ht == NULL || key == NULL || strcmp(key, "") == 0 || value == NULL)
 		return (0);
-
+	value_dup = strdup(value);
+	if (value_dup == NULL)
+		return (0);
 	hash_index = key_index((const unsigned char *) key, (*ht).size);
 	current_node = (*ht).array[hash_index];
 	while (current_node != NULL)
@@ -23,7 +26,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		if (strcmp(key, (*current_node).key) == 0)
 		{
 			free((*current_node).value);
-			(*current_node).value = strdup(value);
+			(*current_node).value = value_dup;
 			return (1);
 		}
 		current_node = (*current_node).next;
@@ -32,7 +35,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 		return (0);
-	(*new_node).value = strdup(value);
+	(*new_node).value = value_dup;
 	(*new_node).key = (char *) key;
 	(*new_node).next = (*ht).array[hash_index];
 	(*ht).array[hash_index] = new_node;
